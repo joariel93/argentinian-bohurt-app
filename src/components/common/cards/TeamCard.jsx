f9af038b463936368046eaab4f2c8ee4d4742ebc
joariel93 <joariel93@hotmail.com>
2026-09-14 11:34:39 -0300

    fix en concatenacion de locacion en team card

--- src/components/common/cards/TeamCard.jsx
+++ src/components/common/cards/TeamCard.jsx
@@ -90,7 +90,7 @@ const TeamCard = ({ team }) => {
                         {team.nombre}
                     </div>
                     {team.ciudad && (
-                        <span>{team.ciudad}, {team.provincia}</span>
+                        <span>{team.ciudad} {team.provincia && (`, ${team.provincia}`)}</span>
                     )}
                 </div>
                 <div className="flex align-items-center justify-content-around p-3">
